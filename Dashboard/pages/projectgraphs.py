7858e8fe7c6dac859afd2652dd369454cf0702f3
shivendra verma <vs6828208@gmail.com>
2025-04-16 23:27:30 +0530

    assignment

---
+++ Dashboard/pages/projectgraphs.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+df = pd.read_csv(r"C:/Users/vs682/OneDrive/Desktop/DATA ANALYTICS/Dashboard/alzheimers_prediction_dataset.csv")
+
+
+print(df)
+
